38fea6810a74716b9b1294546ac8f8125849b77d
zhoums <zms825@163.com>
2024-09-29 15:37:01 +0800

    suit for css-library

--- packages/shared/src/imports/getImports.ts
+++ packages/shared/src/imports/getImports.ts
@@ -1,60 +1,83 @@
-import { createRequire } from 'node:module'
-import { parseTemplate, TemplateMatch } from './parseTemplate'
-import { isObject } from '../'
-import type { Options } from '../'
+/** @format */
 
-const require = createRequire(import.meta.url)
+import { createRequire } from 'node:module';
+import { parseTemplate, TemplateMatch } from './parseTemplate';
+import { isObject } from '../';
+import type { Options } from '../';
 
-const importMap = require('vuetify/dist/json/importMap.json') as typeof import('vuetify/dist/json/importMap.json')
-const importMapLabs = require('vuetify/dist/json/importMap-labs.json') as typeof import('vuetify/dist/json/importMap-labs.json')
+const require = createRequire(import.meta.url);
 
-export function getImports (source: string, options: Options) {
-  const { components, directives } = parseTemplate(source)
-  const resolvedComponents: TemplateMatch[] = []
-  const resolvedDirectives: TemplateMatch[] = []
-  const imports = new Map<string, string[]>()
-  const ignore = (isObject(options.autoImport) && options.autoImport.ignore) || null
-  const includeLabs = isObject(options.autoImport) && options.autoImport.labs
+const importMap =
+  require('css-vuetify/dist/json/importMap.json') as typeof import('css-vuetify/dist/json/importMap.json');
+const importMapLabs =
+  require('css-vuetify/dist/json/importMap-labs.json') as typeof import('css-vuetify/dist/json/importMap-labs.json');
+
+export function getImports(source: string, options: Options) {
+  const { components, directives } = parseTemplate(source);
+  const resolvedComponents: TemplateMatch[] = [];
+  const resolvedDirectives: TemplateMatch[] = [];
+  const imports = new Map<string, string[]>();
+  const ignore =
+    (isObject(options.autoImport) && options.autoImport.ignore) || null;
+  const includeLabs = isObject(options.autoImport) && options.autoImport.labs;
   const map = includeLabs
     ? {
-      components: { ...importMap.components, ...importMapLabs.components },
-      directives: importMap.directives,
-    }
-    : importMap
+        components: { ...importMap.components, ...importMapLabs.components },
+        directives: importMap.directives
+      }
+    : importMap;
 
   if (components.size || directives.size) {
     components.forEach(component => {
-      if (ignore?.includes(component.name as any)) return
+      if (ignore?.includes(component.name as any)) return;
       if (component.name in importMap.components) {
-        resolvedComponents.push(component)
+        resolvedComponents.push(component);
       } else if (includeLabs && component.name in importMapLabs.components) {
-        resolvedComponents.push(component)
+        resolvedComponents.push(component);
       }
-    })
+    });
     directives.forEach(directive => {
-      if (importMap.directives.includes(directive.name) && !ignore?.includes(directive.name as any)) {
-        resolvedDirectives.push(directive)
+      if (
+        importMap.directives.includes(directive.name) &&
+        !ignore?.includes(directive.name as any)
+      ) {
+        resolvedDirectives.push(directive);
       }
-    })
+    });
   }
 
   resolvedComponents.forEach(component => {
-    addImport(imports, component.name, component.symbol, 'vuetify/lib/' + (map.components as any)[component.name].from)
-  })
+    addImport(
+      imports,
+      component.name,
+      component.symbol,
+      'vuetify/lib/' + (map.components as any)[component.name].from
+    );
+  });
   resolvedDirectives.forEach(directive => {
-    addImport(imports, directive.name, directive.symbol, 'vuetify/lib/directives/index.mjs')
-  })
+    addImport(
+      imports,
+      directive.name,
+      directive.symbol,
+      'vuetify/lib/directives/index.mjs'
+    );
+  });
 
   return {
     imports,
     components: resolvedComponents,
-    directives: resolvedDirectives,
-  }
+    directives: resolvedDirectives
+  };
 }
 
-function addImport (imports: Map<string, string[]>, name: string, as: string, from: string) {
+function addImport(
+  imports: Map<string, string[]>,
+  name: string,
+  as: string,
+  from: string
+) {
   if (!imports.has(from)) {
-    imports.set(from, [])
+    imports.set(from, []);
   }
-  imports.get(from)!.push(`${name} as ${as}`)
+  imports.get(from)!.push(`${name} as ${as}`);
 }
